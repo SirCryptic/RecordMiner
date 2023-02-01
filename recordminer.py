55470d8ff2cce3e51822c25469894058a6bc67e2
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2023-02-01 17:48:21 +0000

    supported file formats (CSV, SQL, JSON,TXT)
    searches offline records in different file formats (CSV, SQL, JSON, and plain text) in a given folder, based on a name, date of birth, and address also allows you to save results to text file for future analysis also supports multithreading.

---
+++ recordminer.py
@@ -0,0 +1,91 @@
+import os
+import csv
+import sqlite3
+import json
+import concurrent.futures
+
+# Developer: SirCryptic (NullSecurityTeam)
+# Info: RecordMiner 1.0
+# dly4evarjw
+# Fuck The System Before It Fucks You!
+os.system('cls' if os.name == 'nt' else 'clear')
+banner = '''
+________                        ______________  _______                    
+___  __ \_____________________________  /__   |/  /__(_)___________________
+__  /_/ /  _ \  ___/  __ \_  ___/  __  /__  /|_/ /__  /__  __ \  _ \_  ___/
+_  _, _//  __/ /__ / /_/ /  /   / /_/ / _  /  / / _  / _  / / /  __/  /    
+/_/ |_| \___/\___/ \____//_/    \__,_/  /_/  /_/  /_/  /_/ /_/\___//_/     
+                                                                           
+'''
+
+print(banner)
+
+def search_file(file_path, name, dob, address):
+    results = []
+    if file_path.endswith(".csv"):
+        with open(file_path, "r") as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            for row in reader:
+                if name in row and (not dob or dob in row) and (not address or address in row):
+                    results.append(dict(zip(headers, row)))
+    elif file_path.endswith(".txt"):
+        with open(file_path, "r") as f:
+            for line in f:
+                if name in line and (not dob or dob in line) and (not address or address in line):
+                    results.append({"content": line})
+    elif file_path.endswith(".sql"):
+        conn = sqlite3.connect(file_path)
+        c = conn.cursor()
+        query = f"SELECT * from records where name like '%{name}%'"
+        if dob:
+            query += f" and dob like '%{dob}%'"
+        if address:
+            query += f" and address like '%{address}%'"
+        c.execute(query)
+        rows = c.fetchall()
+        headers = [desc[0] for desc in c.description]
+        for row in rows:
+            results.append(dict(zip(headers, row)))
+        conn.close()
+    elif file_path.endswith(".json"):
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            for record in data:
+                if name in record.values() and (not dob or dob in record.values()) and (not address or address in record.values()):
+                    results.append(record)
+    return results
+
+def main(folder_location):
+    name = input("Enter the name to search: ")
+    dob = input("Enter the date of birth (optional): ")
+    address = input("Enter the address (optional): ")
+
+    results = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        filenames = os.listdir(folder_location)
+        future_to_filename = {executor.submit(search_file, os.path.join(folder_location, filename), name, dob, address): filename for filename in filenames}
+        for future in concurrent.futures.as_completed(future_to_filename):
+            results.extend(future.result())
+
+    if results:
+        save = input("Do you want to save the results to a text file (yes/no)? ")
+        if save.lower() == "yes":
+            with open(f"{name}.txt", "w") as f:
+                for result in results:
+                    f.write(str(result))
+                    f.write("\n")
+                    
+        print("Results:")
+        for result in results:
+            print(result)
+    else:
+        print("No results found.")
+
+if __name__ == "__main__":
+    folder_location = input("Enter the folder location: ")
+    while not os.path.isdir(folder_location):
+        print(f"Error: '{folder_location}' is not a valid directory.")
+        folder_location = input("Enter the folder location: ")
+
+    main(folder_location)
